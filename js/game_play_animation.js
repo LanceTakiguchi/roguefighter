fe9ac305c38ec73ccb1e926c4ab975396f93dee8
bournecreative <bournecreative@gmail.com>
2016-10-25 19:02:59 -0700

    animation for game play introduction and updated hero image added

---
+++ js/game_play_animation.js
@@ -0,0 +1,12 @@
+$(document).ready(clickhander);
+
+function clickhander(){
+  $('p').click(cleanScroll);
+  setTimeout(cleanScroll, 30000);
+}
+
+function cleanScroll() {
+  $('html, body').animate({
+    scrollTop: $("#gameArea").offset().top
+  }, 3000);
+}
