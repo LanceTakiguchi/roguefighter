892501ad25db6d49706186aa07f21ccdbd167423
Collette-Tamez <cttmz4@gmail.com>
2016-10-26 12:42:50 -0700

    can theoretically charge a card

--- stripe_handler.php
+++ stripe_handler.php
@@ -1,12 +1,21 @@
 <?php
-require_once('mysql_connect.php');
+//require_once('mysql_connect.php');
 require_once ('stripe-php-4.1.0/init.php');
-
-\Stripe\Stripe::setApiKey('sk_test_k7htWlvB4XQJXy9qzNydieSo');
-$myCard = array('number' => '4242424242424242', 'exp_month' => 8, 'exp_year' => 2018);
-$charge = \Stripe\Charge::create(array('card' => $myCard, 'amount' => 2000, 'currency' => 'usd'));
-echo $charge;
-
-
-//print_r($_POST);
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+\Stripe\Stripe::setApiKey("sk_test_k7htWlvB4XQJXy9qzNydieSo");
+// Get the credit card details submitted by the form
+$token = $_POST['stripeToken'];
+// Create a charge: this will charge the user's card
+try {
+    $charge = \Stripe\Charge::create(array(
+        "amount" => 1000, // Amount in cents
+        "currency" => "usd",
+        "source" => $token,
+        "description" => "Example charge"
+    ));
+    print_r("charge successful");
+} catch(\Stripe\Error\Card $e) {
+    echo  "The card has been declined";
+}
 ?>
