f8fd3398de4b0f7c62b28f454aff7f5248b12905
Collette-Tamez <cttmz4@gmail.com>
2016-10-26 11:58:09 -0700

    now add user to database upon first login

--- login_handler.php
+++ login_handler.php
@@ -10,18 +10,26 @@ if(!$conn){
 }
 $user_name = $_POST['userName'];
 $user_email = $_POST['userEmail'];
-$query_string = "SELECT `username`, `high_score` FROM `users` WHERE `email`='$user_email'";
-if (isset($user_email)){
-    $query_result = mysqli_query($conn, $query_string);
-    if(mysqli_num_rows($query_result) > 0){
-        $output['success'] = true;
-        $output['message'] = "successfully logged in";
-        while ($row = mysqli_fetch_assoc($query_result)){
-            $output['data'] = $row;
-        }
-    } else{
+if(isset($user_email)){
+    $user_search = "SELECT `id` FROM  `users` WHERE `email`='$user_email'";
+    $user_search_result = mysqli_query($conn, $user_search);
+    if(mysqli_num_rows($user_search_result) === 0) {
         $user_insert_query = "INSERT INTO `users`(`username`, `email`, `high_score`, `date_created`) VALUES ('$user_name','$user_email','0','NOW()')";
         $insert_query_result = mysqli_query($conn, $user_insert_query);
+    }
+} else {
+    $output['message'] = "USERNAME OR PASSWORD INCORRECT";
+    $fatal_error = json_encode($output);
+    print_r($fatal_error);
+    exit();
+}
+$grab_user_data = "SELECT `username`, `high_score` FROM `users` WHERE `email`='$user_email'";
+$user_data_results = mysqli_query($conn, $grab_user_data);
+if(mysqli_num_rows($user_data_results) > 0) {
+    $output['success'] = true;
+    $output['message'] = "successfully logged in";
+    while ($row = mysqli_fetch_assoc($user_data_results)) {
+        $output['data'] = $row;
     }
 } else {
     $output['message'] = "USERNAME OR PASSWORD INCORRECT";
