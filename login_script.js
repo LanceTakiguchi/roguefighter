d897d490dea3c07c8ddcf351572af3e34e78cb6b
Collette-Tamez <cttmz4@gmail.com>
2016-11-01 14:36:04 -0700

    commit unsaved changes

--- login_script.js
+++ login_script.js
@@ -1,16 +1,4 @@
-$(document).ready(function () {
-    addClickHandlers();
-});
-function addClickHandlers() {
-    $("#id").click(function () {
-        makeAjaxCall();
-    });
-}
 function makeAjaxCall(dataobj) {
-    // var dataToSend = {
-    //     userEmail: $("input:first-child").val(),
-    //     password: $(":password").val()
-    // };
     $.ajax({
         url: "login_handler.php",
         data: dataobj,
@@ -22,6 +10,7 @@ function makeAjaxCall(dataobj) {
             $("<h1>").text(responseObj.message).appendTo("#welcomeBack");
             $("<h2>").text("Welcome back " + responseObj.data.username).appendTo("#welcomeBack");
             $("<h3>").text("Your highscore is: " + responseObj.data.high_score).appendTo("#welcomeBack");
+            $("<h4>").text("Number of lives remaining is: " + responseObj.data.game_plays).appendTo("#welcomeBack");
         },
         error: function (response) {
             console.log(response);
@@ -31,21 +20,21 @@ function makeAjaxCall(dataobj) {
 ///////
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    //console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
+    //console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
     var dataToSend = {
         userEmail: profile.getEmail(),
         userName: profile.getName()
     };
     makeAjaxCall(dataToSend);
-    $("<img>", {src: profile.getImageURl()}).apppendTo("#welcomeBack");
+    //$("<img>", {src: profile.getImageURl()}).apppendTo("#welcomeBack");
 }
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
+        $("#welcomeBack").empty();
     });
-    $("#welcomeBack").empty();
 }
